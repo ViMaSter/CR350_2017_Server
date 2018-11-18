0a971bad1cb2262dcb86a7ac651cc8a9ac66713a
Vincent Mahnke <vincent@mahn.ke>
2018-11-18 12:08:20 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import { SessionServer } from "SessionServer"
+import { ISessionData } from "SessionServer"
+
+class GameData implements ISessionData
+{
+	playerPositionX : number = -1;
+	playerPositionY : number = -1;
+	health : number = -1;
+	constructor(parameters : any)
+	{
+		if (typeof parameters.playerPositionX == "number")
+		{
+			this.playerPositionX = parameters.playerPositionX;
+		}
+		if (typeof parameters.playerPositionY == "number")
+		{
+			this.playerPositionY = parameters.playerPositionY;
+		}
+		if (typeof parameters.health == "number")
+		{
+			this.health = parameters.health;
+		}
+	}
+
+	Update(parameters : any) : void
+	{
+		if (typeof parameters.playerPositionX == "number")
+		{
+			this.playerPositionX = parameters.playerPositionX;
+		}
+		if (typeof parameters.playerPositionY == "number")
+		{
+			this.playerPositionY = parameters.playerPositionY;
+		}
+		if (typeof parameters.health == "number")
+		{
+			this.health = parameters.health;
+		}
+	}
+};
+
+
+let server : Promise<SessionServer> = SessionServer.Create(GameData, 7000);
